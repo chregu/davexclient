108f60c65ef0609ca81a98be010c20ba6f426eb8
Christian Stocker <me@chregu.tv>
2010-12-16 07:31:37 +0100

    Added -Xlint:deprecation

--- builandrun.sh
+++ builandrun.sh
@@ -1,6 +1,6 @@
 JACKRABBIT_JAR=/opt/git/jackrabbit/jackrabbit-standalone/target/jackrabbit-standalone-2.2.1-SNAPSHOT.jar
 
- javac -cp  $JACKRABBIT_JAR src/Test.java src/ch/liip/jcr/davex/DavexClient.java  src/ch/liip/jcr/davex/RepositoryConfigImplTest.java src/ch/liip/jcr/davex/AbstractRepositoryConfig.java   
+ javac  -Xlint:deprecation  -cp  $JACKRABBIT_JAR:/opt/git/jackrabbit/jackrabbit-spi2dav/target/jackrabbit-spi2dav-2.2.1-SNAPSHOT.jar src/Test.java src/ch/liip/jcr/davex/DavexClient.java  src/ch/liip/jcr/davex/RepositoryConfigImplTest.java src/ch/liip/jcr/davex/AbstractRepositoryConfig.java   
  cd src 
  jar cf Test.jar Test.class ch/liip/jcr/davex/*.class 
  cd .. 
